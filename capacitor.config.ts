89f081dc4c4e1cda56f09d9cba73e4404e6eb98c
x2loreeh <loreytbs33@gmail.com>
2025-09-08 15:56:35 +0200

    feat: added android packages

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.x2loreeh.x2player',
+  appName: 'x2player',
+  webDir: 'dist/public',
+};
+
+export default config;
